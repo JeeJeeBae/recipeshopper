c8680ba8aae934141cce0dca8daed3e10111cb39
Ji Soo Kim <hello@jisoo.co>
2024-03-17 15:41:43 +0800

    17MarCommit1

--- RecipePlanner/src/Pages/SavedRecipes.jsx
+++ RecipePlanner/src/Pages/SavedRecipes.jsx
@@ -2,15 +2,32 @@ import React from "react";
 
 const SavedRecipes = ({ savedRecipe }) => {
   return (
-    <div>
-      {savedRecipe && (
-        <div>
-          <h3>{savedRecipe.title}</h3>
-          <img src={savedRecipe.image} alt={savedRecipe.title} />
-          <p>Ready in {savedRecipe.time} minutes.</p>
+    <>
+      <div className="savedTitle">Your saved recipes:</div>
+      <div className="SavedCardContainer">
+        <div className="">
+          <div className="savedCard">
+            {savedRecipe && (
+              <div className="savedCardContent">
+                <div className="savedCardImageContainer">
+                  <img
+                    className="savedCardImage"
+                    src={savedRecipe.image}
+                    alt={savedRecipe.title}
+                  />
+                </div>
+                <div className="savedCardTitle">
+                  <h2>{savedRecipe.title}</h2>
+                  <p>Ready in {savedRecipe.time} minutes.</p>
+                </div>
+
+                <button className="deleteButton">delete</button>
+              </div>
+            )}
+          </div>
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 };
 
